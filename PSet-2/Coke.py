3a86a6a2b9cf8064a78de42b14675adbf69eb550
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-01-30 14:43:23 +0530

    Update Coke.py

--- PSet-2/Coke.py
+++ PSet-2/Coke.py
@@ -1,9 +1,9 @@
-''' CODE line by line explanation
-PROBLEM : Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in these
+''' PROBLEM : Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in these
 denominations: 25 cents, 10 cents, and 5 cents. In a file called coke.py, implement a program that prompts the user to insert a coin,
 one at a time, each time informing the user of the amount due. Once the user has inputted at least 50 cents, output how many
 cents in change the user is owed. Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
 
+CODE line by line explanation : 
 • It initially starts with amount due as 50.
 • The allowed inputs are in a list.
 • Prints the initial due amount.
