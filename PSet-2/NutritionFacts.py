95a0a537cc8f2ddd0a9d9b1af7e9b69c0e432562
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-02-02 02:12:37 +0530

    Update NutritionFacts.py

--- PSet-2/NutritionFacts.py
+++ PSet-2/NutritionFacts.py
@@ -2,3 +2,29 @@
 
 Solution : Pretty ez done by using a for loop to traverse thru the entire dict and check if its in the dict and display its value using the func
 '''
+fruits_data = {
+        "apple": "130",
+        "avocado": "50",
+        "banana": "110",
+        "cantaloupe": "50",
+        "grapefruit": "60",
+        "grapes": "90",
+        "honeydew melon": "50",
+        "kiwifruit": "90",
+        "lemon": "15",
+        "lime": "20",
+        "nectarine": "60",
+        "orange": "80",
+        "peach": "60",
+        "pear": "100",
+        "pineapple": "50",
+        "plums": "70",
+        "strawberries": "50",
+        "sweet cherries": "100",
+        "tangerine": "50",
+        "watermelon": "80"
+        }
+fruit=input("Item :").lower()
+for i in fruits_data:
+    if i in fruits_data and i==fruit:
+        print(f"Calories: {fruits_data[i]}")
