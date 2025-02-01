97f52e341dac0b06236d4cb408bc852342ce49b3
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-02-02 02:10:50 +0530

    Create NutritionFacts.py

---
+++ PSet-2/NutritionFacts.py
@@ -0,0 +1,4 @@
+'''Problem : To find out if the given fruit is in the given list and print its calories
+
+Solution : Pretty ez done by using a for loop to traverse thru the entire dict and check if its in the dict and display its value using the func
+'''
