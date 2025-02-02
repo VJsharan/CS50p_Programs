0d2adeca65e004127ce86928b04a3a4ff40fed1b
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-02-02 13:51:30 +0530

    Update Fuel.py

--- PSet-3/Fuel.py
+++ PSet-3/Fuel.py
@@ -1,1 +1,29 @@
 
+'''frac=input("Fraction :")
+x=int(frac[0])
+y=int(frac[-1])
+ans=x/y*100
+if x==y:
+    print("F")
+elif x>y
+    '''
+'''
+REQUIREMENTS:
+IF NUMERATOR
+'''
+def main():
+    given=input("Fraction: ")
+    print(frac(given))
+
+
+def frac(given):
+    while True:
+        try:
+            x,y=given.split('/')
+            if int(x)>int(y)
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+main()
+
