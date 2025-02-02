7dec951d72b2b4fb5ea5de71872d8f33244fd36d
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-02-03 01:57:18 +0530

    Update taqueria.py

--- PSet-3/taqueria.py
+++ PSet-3/taqueria.py
@@ -1,1 +1,30 @@
+'''#problem : to enter the food, and one CTRL + D is pressed, it has to display the total amount
+# Solution : 
+
+'''
+import sys
+val=0.00
+given = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+while True:
+    try:
+        item = input("Item: ").title()
+        if item=='':
+            continue
+        if item in given:
+            val+=float(given[item])
+            print(f"\nTotal ${val:.2f}")
+    except KeyError:
+        pass
+    except EOFError:
+        sys.exit()
 
