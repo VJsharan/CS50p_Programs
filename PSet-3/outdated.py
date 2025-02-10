09c44737b140708e2e4941380732bbe5c3304b92
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-02-11 01:11:51 +0530

    Update outdated.py

--- PSet-3/outdated.py
+++ PSet-3/outdated.py
@@ -1,1 +1,34 @@
+months_list = [
+    "xtra",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
+while True:
+    given=input("Date: ")
+    if '/' in given:
+        month,day,year=given.split('/')
+        year=year.strip()
+        if day.isdigit() and month.isdigit():
+            if int(day)<=31 and int(month)<=12:
+                    print(f"{year}-{int(month):02}-{int(day):02}")
+                    break
+    elif " " and "," in given:
+        month,day,year=given.split(' ')
+        day=day.replace(',','')
+        if day.isdigit() and month.isalpha():
+                if int(day)<=31 and month in months_list:
+                    print(f"{year.strip()}-{months_list.index(month):02}-{int(day):02}")
+                    break
+    else:
+        continue
